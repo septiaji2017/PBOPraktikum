5dff61a1e033320e9b20389063728e301cdeb683
Ridha Septiaji <73025651+septiaji2017@users.noreply.github.com>
2023-09-02 15:07:04 +0700

    Pengumpulan tugas kedua

---
+++ Penugasan 2/Soal 3/Soal3Clean.java
@@ -0,0 +1,8 @@
+public class Soal3Clean {
+
+    public static void main(String[] args) {
+        double x = 93.49;
+        int nx = (int) Math.round(x);
+        System.out.println(nx);
+    }
+}
